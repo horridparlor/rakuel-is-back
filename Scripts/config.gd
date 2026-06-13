5176c5161b28f0898c9ebd711f7cf5c7c5f9cbd8
Eero Laine <eero@rakuel.com>
2026-06-13 23:16:38 +0300

    Eru and piker added

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -2,5 +2,5 @@ extends Node
 
 const VERSION : String = "v0.1";
 const MAX_CARD_ID : int = 60;
-const STARTING_CARD_ID : int = 20;
+const STARTING_CARD_ID : int = 30;
 const PRINT_MODE : bool = true;
