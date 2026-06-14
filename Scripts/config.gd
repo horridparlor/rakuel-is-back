39e32bf75654831180425986d482dfd267314545
Eero Laine <eero@rakuel.com>
2026-06-14 16:12:03 +0300

    LittG added

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -2,5 +2,5 @@ extends Node
 
 const VERSION : String = "v0.1";
 const MAX_CARD_ID : int = 60;
-const STARTING_CARD_ID : int = 38;
+const STARTING_CARD_ID : int = 39;
 const PRINT_MODE : bool = true;
