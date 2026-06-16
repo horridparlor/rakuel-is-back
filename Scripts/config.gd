3f6e4a880970c44e7ffc72fb79d8ef993a4241b4
Eero Laine <eero@rakuel.com>
2026-06-16 13:57:41 +0300

    ToiletPaper and UnicornHorns added

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -2,5 +2,5 @@ extends Node
 
 const VERSION : String = "v0.1";
 const MAX_CARD_ID : int = 60;
-const STARTING_CARD_ID : int = 58;
+const STARTING_CARD_ID : int = 53;
 const PRINT_MODE : bool = true;
