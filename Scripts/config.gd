f3daba9673bb3415054ead0f1cbb8fdb6cf7c821
Eero Laine <eero@rakuel.com>
2026-06-22 17:28:19 +0300

    Skultella and Dalin

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -1,6 +1,6 @@
 extends Node
 
 const VERSION : String = "v0.1";
-const MAX_CARD_ID : int = 60;
-const STARTING_CARD_ID : int = 53;
+const MAX_CARD_ID : int = 62;
+const STARTING_CARD_ID : int = 62;
 const PRINT_MODE : bool = true;
