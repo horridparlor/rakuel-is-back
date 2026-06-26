392ab297c353fec18bbd96edef0bbbf90cb67361
Eero Laine <eero@rakuel.com>
2026-06-26 21:55:48 +0300

    Set2 keywords fixed

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -2,6 +2,6 @@ extends Node
 
 const VERSION : String = "v0.1";
 const MAX_CARD_ID : int = 123;
-const STARTING_CARD_ID : int = 64;
+const STARTING_CARD_ID : int = 84;
 const PRINT_MODE : bool = true;
 const SCREENSHOT_ALL : bool = false;
