0f71f32f3646c08d6040e4f343f58bddcc121681
Eero Laine <eero@rakuel.com>
2026-07-14 01:04:49 +0300

    Toppu, DigitalOnly added cards (2)

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -2,6 +2,6 @@ extends Node
 
 const VERSION : String = "v0.1";
 const MAX_CARD_ID : int = 123;
-const STARTING_CARD_ID : int = 67;
+const STARTING_CARD_ID : int = 108;
 const PRINT_MODE : bool = true;
 const SCREENSHOT_ALL : bool = false;
