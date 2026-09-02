2ddb69ccc606f7796215d00f1dbecb81040e3afd
Eero Laine <eero@rakuel.com>
2026-09-02 23:16:09 +0300

    Treasure with Diamond

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -2,6 +2,6 @@ extends Node
 
 const VERSION : String = "v0.1";
 const MAX_CARD_ID : int = 123;
-const STARTING_CARD_ID : int = 88;
+const STARTING_CARD_ID : int = 81;
 const PRINT_MODE : bool = true;
 const SCREENSHOT_ALL : bool = false;
