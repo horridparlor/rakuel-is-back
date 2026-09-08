72afba0410f9e161a255efba206aa8db1d1829c8
Eero Laine <eero@rakuel.com>
2026-09-08 13:26:08 +0300

    Fixed Keyword font_size getting smaller

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -2,6 +2,6 @@ extends Node
 
 const VERSION : String = "v0.1";
 const MAX_CARD_ID : int = 123;
-const STARTING_CARD_ID : int = 90;
+const STARTING_CARD_ID : int = 44;
 const PRINT_MODE : bool = true;
 const SCREENSHOT_ALL : bool = false;
