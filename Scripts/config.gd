570930067a9e20db717bc129d7172aa170a54160
Eero Laine <eero@rakuel.com>
2026-09-10 02:11:04 +0300

    Added descriptions for new set 3 keywords

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -2,6 +2,6 @@ extends Node
 
 const VERSION : String = "v0.1";
 const MAX_CARD_ID : int = 180;
-const STARTING_CARD_ID : int = 125;
+const STARTING_CARD_ID : int = 151;
 const PRINT_MODE : bool = true;
 const SCREENSHOT_ALL : bool = false;
