0cb5a33ccd1957282f1601ef9b5b793c625f5dc8
Eero Laine <eero@rakuel.com>
2026-09-10 00:52:30 +0300

    Default enum for all the new 26 keywords

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -2,6 +2,6 @@ extends Node
 
 const VERSION : String = "v0.1";
 const MAX_CARD_ID : int = 180;
-const STARTING_CARD_ID : int = 173;
+const STARTING_CARD_ID : int = 125;
 const PRINT_MODE : bool = true;
 const SCREENSHOT_ALL : bool = false;
